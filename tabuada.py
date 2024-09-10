4f92d0cf1b70cb8e77053f6f4ff2e8392f1cf1a6
Marcelo <marcelo.cavalcanti@mv.com.br>
2024-09-10 15:28:06 -0300

    refactory tabuada

--- tabuada.py
+++ tabuada.py
@@ -1,18 +1,25 @@
 #!usr/bin/env python
 
 """ Imprime a tabuada do 1 ao 10 """
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "Marcelo"
 
+template = """
+
+---TABUADA ---
+
+{bloco:^18}
+
+##################
+"""
 
 #base = [1,2,3,4,5,6,7,8,9,10]
 numeros = list(range(1,11))
 
-for numero in numeros:
-    print("Tabuada do:", numero)
-    for o_numero in numeros:
-        print(numero * o_numero)
-
- 
-print(numero)
+for n1 in numeros:
+    print("{:-^18}".format("Tabuada"))
+    for n2 in numeros:
+        resultado = n1 * n2
+        print("{:^18}".format(f"{n1} x {n2} = {resultado}"))    
+    print("#" * 18) 
 
