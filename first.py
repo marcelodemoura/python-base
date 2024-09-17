8cc062b5dd92e8dbe8949e8fa36e57b3c5eae42d
Marcelo <marcelo.cavalcanti@mv.com.br>
2024-09-17 10:05:40 -0300

    versao 0.1.3 first.py

--- first.py
+++ first.py
@@ -15,10 +15,38 @@ __version__ = "0.1.2"
 __author__ = "Marcelo de Moura"
 
 import os
+import sys
 
-current_language = os.getenv("LANG","en_US")
+arguments = {
+    "lang":None,
+    "count":3,
+}
+
+for arg in sys.argv[1:]:
+   #TODO:Tratar ValueError
+   key, value = arg.split("=")
+   key = key.lstrip("-").strip()
+   value = value.strip()
+   if key not in arguments:
+       print(f"Invalid option '{key}'")
+       sys.exit()
+   arguments[key] = value
+
+#current_language = os.getenv("LANG","en_US")
+#argsuments = {}
+#for arg in sys.argv[1:]:
+#    print(f"{sys.argv=}")
 
 #current_language = "it_IT"
+
+#current_language = current_language[:5]
+
+current_language = arguments["lang"]  
+if current_language is None:
+    current_language = os.getenv("LANG","en_US")
+    #TODO: repeticao
+    if current_language is None:
+        current_language = input("Qual linguagem utilizada:")
 
 msg = {
     "en_US": "Hello word!",
@@ -28,4 +56,5 @@ msg = {
     "fr_FR": "Bonjour, Monde",
 }
 
+#print(msg[current_language] * arguments ["count"])
 print(msg[current_language])
