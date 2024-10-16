2e7463c60d60c15f769b1440e427fab003c17eea
Marcelo <marcelo.cavalcanti@mv.com.br>
2024-10-16 09:47:09 -0300

    refactor

--- interpolacao.py
+++ interpolacao.py
@@ -1,25 +1,31 @@
-email_tmpl = """
-    Olá, %(nome)s
-    
-    Tem interesse em comprar %(produto)s?
-    
-    %(texto)s
-    
-    clique agora no %(link)s
-    
-    Apenas %(quantidades)d disponiveis!
-    
-    Preço promocional %(preco).2f
-    """
-for cliente in clientes:
-    print (
-        email_tmpl
+import os
+import sys 
+
+arguments =sys.argv[1:]
+if not arguments:
+    print("Informar o nome do arquivo de emails")
+    sys.exit(1)
+
+filename = arguments[0]
+templatename = arguments[1]
+
+path = os.curdir
+filepath = os.path.join(path, filename)
+templatepath = os.path.join(path, templatename)
+
+for line in open(filepath):
+    name, email = line.split(",")
+    print(f"Enviando email para: {email}")
+    print()
+    print (open(templatepath).read()
             % {
-                "nome": cliente,
+                "nome": name,
                 "produto": "caneta",
                 "texto": "Escreve muito bem",
                 "link": "https://cantaslegais.com",
                 "quantidade": 1,
                 "preco": 50.5,
+                
     }
 )
+print("-" * 50)
