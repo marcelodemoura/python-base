d3d03d2fe4cd5aabf8f414953590ae0db874b3c4
Marcelo <marcelo.cavalcanti@mv.com.br>
2024-10-17 11:40:06 -0300

    tratamento de erros

--- errors.py
+++ errors.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
+import os
+import sys
 
-#names =["joao", "jose", "maria"]
-#print(names[1])
+#EAFP - easy to ask Foring than permission
+try:
+    names = open("names.txt").readlines()
+except:
+    print("[Error] File names.txt not found.")
+    sys.exit(1)
 
-names = open("names.txt").readlines()
-print(names[4])
+try:
+    print(names[5])
+except:
+    print("[Error ] missing name in the list")
+    sys.exit(1)
