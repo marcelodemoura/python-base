7aa678ac954d914c4bc2bc84d38af4989456b812
Marcelo <marcelo.cavalcanti@mv.com.br>
2024-10-17 11:52:01 -0300

    exemplos

--- errors.py
+++ errors.py
@@ -3,14 +3,24 @@ import os
 import sys
 
 #EAFP - easy to ask Foring than permission
+#Exemplos
 try:
     names = open("names.txt").readlines()
-except:
+    2/2
+    print(names.append)
+except FileNotFoundError:
     print("[Error] File names.txt not found.")
     sys.exit(1)
+except ZeroDivisionError:
+    print("[Error] You cant divide by zero!!!")
+    sys.exit(1)
+except AttributeError:
+    print("[Error] List doesn't have banana")
+    sys.exit(1)
+
 
 try:
-    print(names[5])
+    print(names[3])
 except:
-    print("[Error ] missing name in the list")
+    print("[Error Missing nae in the list] ")
     sys.exit(1)
