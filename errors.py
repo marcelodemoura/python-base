bed25d02d0e7f9fcd2943229e4f17c53d3ac4f0b
Marcelo <marcelo.cavalcanti@mv.com.br>
2024-10-17 14:29:02 -0300

    exemplos

--- errors.py
+++ errors.py
@@ -6,16 +6,8 @@ import sys
 #Exemplos
 try:
     names = open("names.txt").readlines()
-    2/2
-    print(names.append)
-except FileNotFoundError:
-    print("[Error] File names.txt not found.")
-    sys.exit(1)
-except ZeroDivisionError:
-    print("[Error] You cant divide by zero!!!")
-    sys.exit(1)
-except AttributeError:
-    print("[Error] List doesn't have banana")
+except FileNotFoundError as e:
+    print(f"{str(e)}.")
     sys.exit(1)
 
 
